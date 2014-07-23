0f97feddc0930c35bc9791e6d2457bd88ef3eeb1
TheCountryGamer <the.country.gamer@gmail.com>
2014-07-23 19:00:19 -0400

    Created OEFactory (For gui config)

---
+++ src/main/scala/com/countrygamer/owneremitter/client/gui/configFactory/OEFactory.scala
@@ -0,0 +1,13 @@
+package com.countrygamer.owneremitter.client.gui.configFactory
+
+import com.countrygamer.cgo.wrapper.client.gui.configFactory.GuiFactoryWrapper
+import com.countrygamer.owneremitter.common.OEOptions
+
+/**
+ *
+ *
+ * @author CountryGamer
+ */
+class OEFactory() extends GuiFactoryWrapper(OEOptions) {
+
+}
