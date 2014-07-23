e1ed1a2275c5bf4a5b721f64aeb9317ec9ee1c96
TheCountryGamer <the.country.gamer@gmail.com>
2014-07-23 19:58:21 -0400

    Added base TE for the owner emitter block

---
+++ src/main/scala/com/countrygamer/owneremitter/common/tile/TEEmitter.scala
@@ -0,0 +1,12 @@
+package com.countrygamer.owneremitter.common.tile
+
+import com.countrygamer.cgo.wrapper.common.tile.TEWrapper
+
+/**
+ *
+ *
+ * @author CountryGamer
+ */
+class TEEmitter() extends TEWrapper("") {
+
+}
