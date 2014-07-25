3ecd13d188164b5a5fdd4d3e4918703bc117dbc7
TheCountryGamer <the.country.gamer@gmail.com>
2014-07-25 11:18:56 -0400

    Config options for emitters

--- src/main/scala/com/countrygamer/owneremitter/common/OEOptions.scala
+++ src/main/scala/com/countrygamer/owneremitter/common/OEOptions.scala
@@ -1,8 +1,10 @@
 package com.countrygamer.owneremitter.common
 
+import com.countrygamer.cgo.common.lib.util.Config
 import com.countrygamer.cgo.wrapper.common.registries.OptionRegister
 import cpw.mods.fml.relauncher.{Side, SideOnly}
 import net.minecraft.client.gui.GuiScreen
+import net.minecraftforge.common.config.Configuration
 
 /**
  *
@@ -11,7 +13,24 @@ import net.minecraft.client.gui.GuiScreen
  */
 object OEOptions extends OptionRegister {
 
+	var enableOwnerEmitter: Boolean = true
+	var enablePlayerEmitter: Boolean = false
+
 	override def register(): Unit = {
+
+		OEOptions.enableOwnerEmitter = Config
+				.getAndComment(this.config, Configuration.CATEGORY_GENERAL,
+		            "Enable Owner Emitter",
+		            "Enable the owner emitter block",
+		            true
+				)
+
+		OEOptions.enablePlayerEmitter = Config
+				.getAndComment(this.config, Configuration.CATEGORY_GENERAL,
+		            "Enable Player Emitter",
+		            "Enable the player emitter block. WARNING: UNFINISHED!",
+		            true
+				)
 
 	}
 
